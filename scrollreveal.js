e1b2c11ffa806427f2952e734bd6481b1f655a33
geilsonfreire <geilsonfreireleite@gmail.com>
2023-12-20 23:49:35 -0300

    Foi aplicado scroll Raveal em todas as sesções do site

---
+++ scrollreveal.js
@@ -0,0 +1,50 @@
+window.revelar = ScrollReveal({reset: true});
+
+// Topo do site
+revelar.reveal('.scrol-txt-topo', {
+    duration: 2000, origin: 'left', distance: '100px', easing: 'ease-in-out'
+});
+
+revelar.reveal('.scrol-img-topo', {
+    delay: 500, duration: 2000, origin: 'right', distance: '100px', easing: 'ease-in-out'
+});
+
+// Titulo das seções do site
+revelar.reveal('.titulo', {
+    duration: 2000, origin: 'left', distance: '100px', easing: 'ease-in-out'
+});
+
+// skill do site
+revelar.reveal('.scrol-skill1', {
+    delay: 500, duration: 2000, distance: '100px', easing: 'ease-in-out'
+});
+
+revelar.reveal('.scrol-skill2', {
+    delay: 1000, duration: 2000, distance: '100px', easing: 'ease-in-out'
+});
+
+revelar.reveal('.scrol-skill3', {
+    delay: 1500, duration: 2000, distance: '100px', easing: 'ease-in-out'
+});
+
+// Sobre o site
+revelar.reveal('.scrol-img-sobre', {
+    delay: 500, duration: 2000, origin: 'left', distance: '100px', easing: 'ease-in-out'
+});
+
+revelar.reveal('.scrol-txt-sobre', {
+    delay: 1000, duration: 2000, origin: 'right', distance: '100px', easing: 'ease-in-out'
+});
+
+// Projetos do site
+revelar.reveal('.scrol-img-projetos1', {
+    delay: 500, duration: 2000, distance: '100px', easing: 'ease-in-out'
+});
+
+revelar.reveal('.scrol-img-projetos2', {
+    delay: 1000, duration: 2000, distance: '100px', easing: 'ease-in-out'
+});
+
+revelar.reveal('.scrol-img-projetos3', {
+    delay: 1500, duration: 2000, distance: '100px', easing: 'ease-in-out'
+});
