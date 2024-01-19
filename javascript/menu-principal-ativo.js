dc015e120140e6aa301cb239f7407ed36da73c0a
geilsonfreire <geilsonfreireleite@gmail.com>
2024-01-19 00:48:04 -0300

    Foi separado curso complementares e criado a secção skills

--- javascript/menu-principal-ativo.js
+++ javascript/menu-principal-ativo.js
@@ -4,7 +4,7 @@ let navLinks = document.querySelectorAll('header nav a.nav-principal');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
+        let offset = sec.offsetTop - 250;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
