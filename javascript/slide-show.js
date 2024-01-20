06957af771e68dbbd130d5e67398d8b1ea0d8c66
geilsonfreire <geilsonfreireleite@gmail.com>
2024-01-20 00:13:09 -0300

    Foi aplicado a funcionalidade nos Botões pre e next da seção outros cursos

--- javascript/slide-show.js
+++ javascript/slide-show.js
@@ -1,5 +1,7 @@
 // Use const para declarar variáveis que não serão reatribuídas
 const radio = document.querySelector('.manual-btn');
+const prev = document.querySelector('.slide-nav-previous');
+const next = document.querySelector('.slide-nav-next');
 let cont = 1;
 const totalImg = 6;
 
@@ -15,5 +17,26 @@ function proximaImg() {
     if (cont > totalImg) {
         cont = 1;
     }
+    atualizarImg();
+}
+
+// Aplicar evento de click nos botões prev
+prev.addEventListener('click', () => {
+    cont = (cont > 1) ? cont - 1 : totalImg;
+    atualizarImg();
+    console.log('Cliquei em Prev');
+});
+
+// Aplicar evento de click nos botões next
+next.addEventListener('click', () => {
+    cont = (cont < totalImg) ? cont + 1 : 1;
+    atualizarImg();
+    console.log('Cliquei em Next');
+});
+
+// Função para atualizar a exibição da imagem com base no índice
+function atualizarImg() {
     document.getElementById('radio' + cont).checked = true;
 }
+
+
